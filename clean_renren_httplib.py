4dd3163cca0380fd1913d9fe1675b99f7fa4dbdb
lyxint <lyxint@gmail.com>
2012-02-09 18:44:19 +0800

    hide renren log, will mess up log body, *DONT USE*

--- clean_renren_httplib.py
+++ clean_renren_httplib.py
@@ -12,18 +12,62 @@ class Renren(object):
         self.password = password
     
     def login(self):
-        post = {
-            'email': self.username,
-            'password': self.password
-        }
         response = fetch(
             'http://3g.renren.com/login.do?fx=0&autoLogin=true',
-            data = post
+            data = {
+                'email': self.username,
+                'password': self.password
+            },
         )
         
         set_cookie = response.msg.getheaders('set-cookie')
         self.cookies = setcookielist2cookiestring(set_cookie)
-        return self.cookies
+        self.qs = dict(parse_qsl(response.getheader('Location').split('?', 1)[-1]))
+        self.qs['uid'] = self.qs['bm'].split('_', 1)[0]
+        print self.qs
+        return response
+
+    def hide_log(self):
+        prefix = 'http://3g.renren.com/blog/wmyblog.do?id=%s&sid=%s&curpage=%d'
+        logs = []
+        
+        r = fetch(prefix % (self.qs['uid'], self.qs['sid'], 0))
+        logs.extend(re.findall(r'''weditentry.do\?id=(\d+)''', r.body))
+        
+        try: pagetotal = int(re.search(r'''\(第\d+/(\d+)页\)''', r.body).group(1))
+        except: pagetotal = 0
+        
+        for i in xrange(1, pagetotal):
+            r = fetch(prefix % (self.qs['uid'], self.qs['sid'], i))
+            logs.extend(re.findall(r'''weditentry.do\?id=(\d+)''', r.body))
+
+        print 'you got', len(logs), 'logs'
+
+        for log in logs:
+            url = '''http://3g.renren.com/blog/weditentry.do?id=%s&sid=%s''' % (log, self.qs['sid'])
+
+            r = fetch(url)
+            try: 
+                title = re.search(r'''<input name="title".*?value="(.*?)"''', r.body).group(1)
+                body = re.search(r'''<textarea name="body".*?>(.*?)<''', r.body, re.S).group(1)
+                tsc = re.search(r'''<input type="hidden" name="tsc".*?value="(.*?)"''', r.body).group(1)
+                print log, title
+                print tsc
+                r = fetch(
+                    'http://3g.renren.com/blog/weditentry.do?',
+                    data = {
+                        'body': body,
+                        'control': -1,
+                        'id': log,
+                        'passwd': '',
+                        'publish': '发布',
+                        'sid': self.qs['sid'],
+                        'title': title,
+                        'tsc': tsc,
+                    },
+                )
+            except: pass
+            
     
     
     def del_minifeed(self):
@@ -98,5 +142,7 @@ if __name__ == '__main__':
     username, password = sys.argv[1:]
     renren = Renren(username, password)
     renren.login()
-    renren.del_minifeed()
-    renren.del_statuses()
+    renren.hide_log()
+    #renren.del_minifeed()
+    #renren.del_statuses()
+
